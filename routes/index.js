369b7eb7f4ebaad263fa1405e44e8a6c5d803931
ninnemana <ninnemana@gmail.com>
2013-07-23 00:42:07 -0500

    did a little front end work

--- routes/index.js
+++ routes/index.js
@@ -19,10 +19,6 @@ var config = require('../config'),
  	});
 
  	tw.get('users/show',{screen_name: req.session.twitter.user.screen_name},function(err, reply){
- 		console.log(err);
- 		console.log(reply);
- 		res.write('Welcome ' + reply.name);
- 		res.end();
+ 		res.render('index', {title: 'TwitOptic', user: reply});
  	});
-   // res.render('index', { title: 'Express' });  
  };
